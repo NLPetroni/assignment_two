c0dbd448e7b0050e7b9295d88f7fd83228964d6f
Sasso <pietro.peppo@gmail.com>
2021-11-24 14:37:41 +0100

    Create utils.py

---
+++ src/utils.py
@@ -0,0 +1,35 @@
+import os
+import requests
+import zipfile
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+
+def download_data(data_path):
+    toy_data_path = os.path.join(data_path, 'fever_data.zip')
+    toy_data_url_id = "1wArZhF9_SHW17WKNGeLmX-QTYw9Zscl1"
+    toy_url = "https://docs.google.com/uc?export=download"
+
+    if not os.path.exists(data_path):
+        os.makedirs(data_path)
+
+    if not os.path.exists(toy_data_path):
+        print("Downloading FEVER data splits...")
+        with requests.Session() as current_session:
+            response = current_session.get(toy_url,
+                                   params={'id': toy_data_url_id},
+                                   stream=True)
+        save_response_content(response, toy_data_path)
+        print("Download completed!")
+
+        print("Extracting dataset...")
+        with zipfile.ZipFile(toy_data_path) as loaded_zip:
+            loaded_zip.extractall(data_path)
+        print("Extraction completed!")
+
+download_data('dataset')
