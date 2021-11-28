5ea45b80dde29f0bd7ef3bc13177b99ddd3b8525
Sasso <pietro.peppo@gmail.com>
2021-11-28 12:17:01 +0100

    add_oov

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import zipfile
+import numpy as np
 
 def save_response_content(response, destination):
     CHUNK_SIZE = 32768
@@ -31,3 +32,27 @@ def download_data(data_path):
         with zipfile.ZipFile(toy_data_path) as loaded_zip:
             loaded_zip.extractall(data_path)
         print("Extraction completed!")
+        
+def add_oov(start_voc, oovs, embedding_matrix, sentences):
+  """
+    Computes new embedding matrix, adding embeddings for oovs
+    Parameters:
+      start_voc: dict, starting vocabulary that is extended with oovs
+      oovs: set of string, oovs to add to the starting vocabulary and embedding matrix
+      embbedding_matrix: starting embedding matrix (numpy)
+      sentences: list of list of strings, set used to compute oov embeddings
+    Returns tuple (voc, emb) where voc is dict from words to idx (in emb) and emb is (numpy) embedding matrix with oovs
+  """
+  oovs = oovs - set(start_voc.keys())
+  start_voc_size, emb_size = embedding_matrix.shape
+  oov_embeddings = np.zeros((start_voc_size + len(oovs), emb_size))
+  oov_embeddings[:start_voc_size] = embedding_matrix
+  new_voc = dict(start_voc)
+
+  for i, oov in enumerate(oovs):
+    context_words = [new_voc[word] 
+                    for sentence in filter(lambda s: oov in s, sentences)
+                    for word in sentence if word in new_voc and word not in (oov, '<PAD>')]
+    oov_embeddings[i] = np.mean(oov_embeddings[context_words])
+    new_voc[oov] = start_voc_size + i
+  return new_voc, oov_embeddings
